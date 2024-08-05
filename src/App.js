e283a0e071a883cdc526fe3905d8d0dbab76d7b3
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-08-05 18:05:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleClick = () => {
+    if (audioRef.current) {
+      audioRef.current.volume = 1.0;
+      audioRef.current.play().then(() => {
+        setIsPlaying(true);
+      }).catch(error => {
+        console.error("Playback prevented by browser. User interaction required.", error);
+      });
+    }
+  };
+
   return (
     <>
-    {/* <center><h2>This is a Homie Check Point !!</h2></center> */}
-    <center><h2>Eda mone happy alle ??</h2></center>
-    <br></br>
-    <div className="wrapper">
-      <img src="/images/pedro.png" alt="Pedro" className="pedro" />
-      <audio autoPlay loop>
+      <center><h2>Eda mone happy alle ??</h2></center>
+      <br />
+      <div className="wrapper">
+        <img 
+          src="/images/pedro.png" 
+          alt="Pedro" 
+          className="pedro" 
+          onClick={handleClick} 
+          style={{ cursor: 'pointer' }}
+        />
+      </div>
+      <audio ref={audioRef} loop>
         <source src="/images/pedro_song.mp3" type="audio/mp3" />
         Your browser does not support the audio element.
       </audio>
-    </div>
-    
+      <br></br>
+      <center><h3>Tap pedro !!</h3></center>
     </>
   );
 }
